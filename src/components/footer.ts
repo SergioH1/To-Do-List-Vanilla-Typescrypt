fa73a33c512f6c1ba07dde2836344fd63c15aa26
SergioH <sergiohernandezroman@gmail.com>
2022-10-19 11:49:35 +0200

    add p in footer

--- src/components/footer.ts
+++ src/components/footer.ts
@@ -1,17 +1,18 @@
 import { Component } from './component.js';
 
 export class Footer extends Component {
-    template: string;
-    constructor(public selector: string) {
-        super();
-        this.template = this.createTemplate();
-        this.renderAdd(this.selector, this.template);
-    }
-    createTemplate() {
-        return `
+  template: string;
+  constructor(public selector: string) {
+    super();
+    this.template = this.createTemplate();
+    this.renderAdd(this.selector, this.template);
+  }
+  createTemplate() {
+    return `
          <footer>
             <address>ISDI COders</address>
+            <p></p>
         </footer>
         `;
-    }
+  }
 }
